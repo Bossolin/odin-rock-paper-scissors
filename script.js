3947aa2c352da7b3016db5dbf7b1e66d7ba56dbe
Bossolin <vladyslav.okipnyak@icloud.com>
2021-10-30 12:15:28 +0200

    cleaned up the code

--- script.js
+++ script.js
@@ -65,36 +65,3 @@ scissors.addEventListener("click", () => playRound("SCISSORS"));
 const results = document.querySelector(".result");
 const computerDisplay = document.querySelector(".computer");
 const score = document.querySelector(".score");
-
-// function game() {
-//   let playerScore = 0;
-//   let computerScore = 0;
-//   let outcome = "";
-
-//   for (let i = 0; i < 5; i++) {
-//     console.log(`Game number ${i + 1}!`);
-//     outcome = playRound();
-
-//     if (outcome == "That's not an option. Try again!") {
-//       --i;
-//       console.log(outcome);
-//     } else if (outcome == "Player won!") {
-//       ++playerScore;
-//       console.log(outcome);
-//       console.log(`The score is: ${playerScore} to ${computerScore}`);
-//     } else if (outcome == "Computer won!") {
-//       ++computerScore;
-//       console.log(outcome);
-//       console.log(`The score is: ${playerScore} to ${computerScore}`);
-//     } else if (outcome == "It's a tie!") {
-//       console.log(outcome);
-//       console.log(`The score is: ${playerScore} to ${computerScore}`);
-//     }
-//   }
-
-//   if (playerScore > computerScore)
-//     return `Player won ${playerScore} to ${computerScore}`;
-//   else if (computerScore > playerScore)
-//     return `Computer won ${computerScore} to ${playerScore}`;
-//   else return `It's a tie! ${playerScore} : ${computerScore}`;
-// }
